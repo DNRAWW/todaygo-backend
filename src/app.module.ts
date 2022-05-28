9511fb8a30c9e999ad9000a14ccc4176b2368153
Kostya_Moguchev <rjcnz222@gmail.com>
2022-05-29 01:00:21 +0300

    modules changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getOrmConfig } from './modules/config/typeorm';
+import { EventModule } from './modules/events/events.module';
 import { UserModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ".env"
-    }), 
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot(getOrmConfig()),
-    UserModule
+    UserModule,
+    EventModule,
   ],
   controllers: [],
   providers: [],
--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../users/users.module';
+import { CommentsController } from './controllers/comments.controller';
+import { EventsController } from './controllers/events.controller';
 import { CommentEntity } from './entities/comment.entity';
 import { EventEntity } from './entities/event.entity';
 import { EventCommentEntity } from './entities/eventComment.entity';
 import { EventParticipantEntity } from './entities/eventParticipant.entity';
+import { CommentsService } from './services/comments.service';
+import { EventCommentsService } from './services/eventComments.service';
+import { EventParticipantsService } from './services/eventParticipants.service';
+import { EventsService } from './services/events.service';
 
 @Module({
   imports: [
@@ -16,7 +22,12 @@ import { EventParticipantEntity } from './entities/eventParticipant.entity';
     ]),
     UserModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [EventsController, CommentsController],
+  providers: [
+    CommentsService,
+    EventCommentsService,
+    EventParticipantsService,
+    EventsService,
+  ],
 })
 export class EventModule {}
